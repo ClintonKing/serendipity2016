0a4fdba1c22dbe401d84404edafc2a9ba1c9f6f8
ClintonKing <clintonjoking@gmail.com>
2016-12-15 10:45:09 -0500

    Try again

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ app.post('/webhook/', function (req, res) {
         let sender = event.sender.id
         if (event.message && event.message.text) {
             let text = event.message.text
+            getStory()
             if (text === 'NPR'){
               sendNPRCarousel(sender)
               continue
